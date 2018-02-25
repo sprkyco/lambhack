60cb81e81d18f432b1a108e0c011a6f4e4d6f1ed
sprkyco <sprkyco@sprkycos-MacBook-Pro.local>
2018-02-25 09:20:35 -0800

    new spara patch

--- main.go
+++ main.go
@@ -10,81 +10,72 @@ import (
 	"github.com/Sirupsen/logrus"
 	sparta "github.com/mweagle/Sparta"
 	runner "github.com/wickett/lambhack/runner"
+	spartaCF "github.com/mweagle/Sparta/aws/cloudformation"
+	spartaAPIG "github.com/mweagle/Sparta/aws/events"
+	gocf "github.com/mweagle/go-cloudformation"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
-func paramVal(keyName string, defaultValue string) string {
-	value := os.Getenv(keyName)
-	if "" == value {
-		value = defaultValue
+type lambhackResponse struct {
+	Message string
+	Request spartaAPIG.APIGatewayRequest
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// lambhack event handler
+func lambhack(ctx context.Context,
+	gatewayEvent spartaAPIG.APIGatewayRequest) (lambhackResponse, error) {
+
+	logger, loggerOk := ctx.Value(sparta.ContextKeyLogger).(*logrus.Logger)
+	if loggerOk {
+		logger.Info("Lambhack structured log message")
 	}
-	return value
+
+	command := gatewayEvent.QueryParams["command"]
+	commandOutput := runner.Run(command)
+	// Return a message, together with the incoming input...
+	return lambhackResponse{
+		Message: fmt.Sprintf("Welcome to lambhack!" + commandOutput),
+		//Request: gatewayEvent,
+	}, nil
 }
 
-var s3Bucket = paramVal("S3_TEST_BUCKET", "arn:aws:s3:::lambhack")
+func lambhackLambdaFunctions(api *sparta.API) []*sparta.LambdaAWSInfo {
+	var lambdaFunctions []*sparta.LambdaAWSInfo
+	lambdaFn := sparta.HandleAWSLambda(sparta.LambdaName(lambhack),
+		lambhack,
+		sparta.IAMRoleDefinition{})
 
-func lambhackEvent(event *json.RawMessage,
-	context *sparta.LambdaContext,
-	w http.ResponseWriter,
-	logger *logrus.Logger) {
+	if nil != api {
+		apiGatewayResource, _ := api.NewResource("/lambhack", lambdaFn)
 
-	//code here for lambhack
-
-	var lambdaEvent sparta.APIGatewayLambdaJSONEvent
-	_ = json.Unmarshal([]byte(*event), &lambdaEvent)
-
-	command := lambdaEvent.QueryParams["args"]
-	output := runner.Run(command)
-	logger.WithFields(logrus.Fields{
-		"Event":   string(*event),
-		"Command": string(command),
-		"Output":  string(output),
-	}).Info("Request received")
-
-	fmt.Fprintf(w, output)
-	time.Sleep(time.Second)
-}
-
-func appendS3Lambda(api *sparta.API, lambdaFunctions []*sparta.LambdaAWSInfo) []*sparta.LambdaAWSInfo {
-	options := new(sparta.LambdaFunctionOptions)
-	options.Timeout = 30
-	lambdaFn := sparta.NewLambda(sparta.IAMRoleDefinition{}, lambhackEvent, options)
-	apiGatewayResource, _ := api.NewResource("/lambhack/{command+}", lambdaFn)
-	apiGatewayResource.NewMethod("GET", http.StatusOK)
-
-	lambdaFn.Permissions = append(lambdaFn.Permissions, sparta.S3Permission{
-		BasePermission: sparta.BasePermission{
-			SourceArn: s3Bucket,
-		},
-		Events: []string{"s3:ObjectCreated:*", "s3:ObjectRemoved:*"},
-	})
+		// We only return http.StatusOK
+		apiMethod, apiMethodErr := apiGatewayResource.NewMethod("GET",
+			http.StatusOK,
+			http.StatusOK)
+		if nil != apiMethodErr {
+			panic("Failed to create /lambhack resource: " + apiMethodErr.Error())
+		}
+		// The lambda resource only supports application/json Unmarshallable
+		// requests.
+		apiMethod.SupportedRequestContentTypes = []string{"application/json"}
+	}
 	return append(lambdaFunctions, lambdaFn)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Return the *[]sparta.LambdaAWSInfo slice
-//
-func spartaLambdaData(api *sparta.API) []*sparta.LambdaAWSInfo {
-
-	var lambdaFunctions []*sparta.LambdaAWSInfo
-	lambdaFunctions = appendS3Lambda(api, lambdaFunctions)
-	return lambdaFunctions
-}
-
+// Main
 func main() {
-	stage := sparta.NewStage("prod")
-	apiGateway := sparta.NewAPIGateway("lambhackAPI", stage)
-	apiGateway.CORSEnabled = true
 
-	//lambda info
-	os.Setenv("AWS_PROFILE", "sparta")
-	os.Setenv("AWS_REGION", "us-east-1")
+	// Register the function with the API Gateway
+	apiStage := sparta.NewStage("prod")
+	apiGateway := sparta.NewAPIGateway("lambhack", apiStage)
 
-	stackName := "LambhackApplication"
+	// Deploy it
+	stackName := spartaCF.UserScopedStackName("lambhack")
 	sparta.Main(stackName,
-		"Lambhack Application",
-		spartaLambdaData(apiGateway),
+		fmt.Sprintf("Sacrificial lambs"),
+		lambhackLambdaFunctions(apiGateway),
 		apiGateway,
-		nil)
-
+		s3Site)
 }
